317db5cb5c9b85f21af44b29fa0b1ee895f2c023
collins-at-203860813766 <58524716+collinsobasuyi@users.noreply.github.com>
2022-11-04 13:57:36 +0000

    Tech test

---
+++ techTest.js
@@ -0,0 +1,19 @@
+// Break and conditions
+let i = 1;
+while (i <= 11) {
+    if (i === 11) break;
+    
+    console.log(i);
+    i++;
+}
+
+// // For and conditions
+for(let x = 1;x <= 10;x++){
+    console.log(x);
+}
+
+// //sort
+let num = [1, 2, 2, 3, 4, 5, 6, 7, 7, 1, 2, 9, 10];
+let uniqueNum = [...new Set(num)].sort((a, b) => a - b);
+
+console.log(uniqueNum);
