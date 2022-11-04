f7f477f29dababfa265be0e81bf1f3fd081e22dc
collins-at-203860813766 <58524716+collinsobasuyi@users.noreply.github.com>
2022-11-04 14:15:04 +0000

    Tech test

--- techTest.js
+++ techTest.js
@@ -14,7 +14,7 @@ for(let x = 1;x <= 10;x++){
 
 // Sort numerically descending, largest to smallest:
 let num = [1, 2, 2, 3, 4, 5, 6, 7, 7, 1, 2, 9, 10];
-let y = num.sort((a, b) => b - a)
-let uniqueNum = [...new Set(y)];
+let sorted = num.sort((a, b) => b - a)
+let descSortedNum = [...new Set(sorted)];
 
-console.log(uniqueNum);
+console.log(descSortedNum);
